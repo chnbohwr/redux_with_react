62b9fe6488e421bc57bfd8ec4a2be1827c373602
Hyman_Chen <hyman_chen@wistron.com>
2015-12-24 11:58:55 +0800

    add delay middleware

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
-import Redux, { createStore, combineReducers } from 'redux';
+import Redux, { createStore, combineReducers, applyMiddleware } from 'redux';
 
 let button = document.getElementById('mybutton');
+let button2 = document.getElementById('mybutton2');
+let button3 = document.getElementById('mybutton3');
 let text = document.getElementById('count');
 
 //=========action part=============//
 const CLICKEVENT = 'CLICKEVENT';
-function clickAction() {
+function clickAction(count = 1, delay = 0) {
     return{
-        type: CLICKEVENT
+        type: CLICKEVENT,
+        count,
+        delay
     };
+}
+
+//=========middleware part=============//
+const delayMiddleware = store => next => action => {
+    if (action.delay) {
+        return setTimeout(() => {
+            next(action);
+        }, action.delay * 1000);
+    } else {
+        next(action);
+    }
 }
 
 //=========reducer part=============//
@@ -18,8 +33,7 @@ let initialState = {
 function clickReducer (state = initialState, action) {
     switch (action.type) {
         case CLICKEVENT:
-            console.log('CLICKEVENT');
-            state.clickCount += 1;
+            state.clickCount += action.count;
             return { ...state };
         default:
             return state;
@@ -27,15 +41,22 @@ function clickReducer (state = initialState, action) {
 }
 
 //=========store part=============//
+let createStoreWithMiddleware = applyMiddleware(delayMiddleware)(createStore);
 let reducers = combineReducers({
     clickState: clickReducer
 });
-let store = createStore(reducers);
+let store = createStoreWithMiddleware(reducers);
 
 //=========view part=============//
-button.addEventListener("click", function(){
+button.addEventListener('click', () => {
     store.dispatch(clickAction())
 });
+button2.addEventListener('click',() => {
+    store.dispatch(clickAction(10));
+});
+button3.addEventListener('click',() => {
+    store.dispatch(clickAction(5, 2));
+})
 store.subscribe(function(){
     text.textContent = store.getState().clickState.clickCount
 })
