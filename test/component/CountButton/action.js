9281e53e881ae3c67f2ab42866f5b32d7fdbd90c
chnbohwr <--global>
2016-01-14 18:21:36 +0800

    add unit test framework

---
+++ test/component/CountButton/action.js
@@ -0,0 +1,12 @@
+import actions, {TYPES} from 'component/CountButton/action.js';
+
+describe('test1', function () {
+    
+    it('good', function () {
+        
+        console.log(TYPES);
+        expect(1).to.equal(1);
+    
+    });
+    
+});
